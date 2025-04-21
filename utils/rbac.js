cbca5dd94909fce179fc06889e04f9bd808d3068
Perp-pexel <perpaman44@gmail.com>
2025-04-21 20:31:50 +0000

    done

---
+++ utils/rbac.js
@@ -0,0 +1,8 @@
+export const permissions = [
+    {
+        role: 'User',
+        actions: {
+
+        }
+    }
+];
--- validator/user.js
+++ validator/user.js
@@ -1,4 +1,3 @@
-import e from "express";
 import Joi from "joi";
 
 export const registerUserValidator = Joi.object({
