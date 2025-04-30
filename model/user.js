5e62a35e86dc3894be9aa0e50db536b563ed5e61
Perp-pexel <perpaman44@gmail.com>
2025-04-30 11:31:00 +0000

    unique

--- model/user.js
+++ model/user.js
@@ -3,10 +3,10 @@ import {Schema, model} from "mongoose";
 
 const userSchema = new Schema({
     name: {type: String, required: true},
-    email: {type: String, required: true},
-    password: {type: String, required: true, unique: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
 }); {
     userSchema.plugin(toJSON);
 }
 
-export const UserModel = model("User", userSchema);
+export const UserModel = model("User", userSchema); 
